9d9aeb157bc0c1fddc873ae68fa2e1a452701abd
forstema <46746804+forstema@users.noreply.github.com>
2019-03-07 14:29:44 +0100

    initial file import

---
+++ index.js
@@ -0,0 +1,23 @@
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+    name: 'logstash_menu_item',
+    uiExports: {
+      links: [
+        {
+          icon: 'plugins/infra/images/logging_mono_white.svg',
+          euiIconType: 'loggingApp',
+          id: 'logstash:dashboard',
+          order: 8002,
+          title: 'Logstash',
+          url: `kibana#/dashboard/16b99730-359d-11e9-8c84-8f31b106450a`,
+        },
+      ],
+    },
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+  });
+}
